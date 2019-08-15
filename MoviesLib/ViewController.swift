b818993f06caec0699c81772499bddbd6d42b29b
Jose Javier Aviles Gomez <jose.aviles@nibspe.com.br>
2019-08-15 19:37:16 -0300

    Initial Commit

---
+++ MoviesLib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Jose Javier on 15/08/19.
+//  Copyright © 2019 JLApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
